225e66aa71fe68a163e052fc7ce29513e04d5efd
Luke Oliff <luke@lukeoliff.com>
2022-06-10 13:19:40 +0100

    trying it out

--- www/nuxt.config.ts
+++ www/nuxt.config.ts
@@ -2,8 +2,9 @@ import { defineNuxtConfig } from "nuxt";
 import { resolve } from "path";
 import { copyFiles } from "../scripts/copyFiles";
 
-copyFiles("../content/www", "public", [".md"]);
-copyFiles("static", "public", []);
+copyFiles("static", "public", []); // anything in /static goes in /public (so we can .gitignore public)
+copyFiles("../static", "public/shared", []); // anything in /static goes in /public/shared (files common to multiple sites)
+copyFiles("../content/www", "public", [".md"]); // anything non-markdown files in ../content/www/ goes in /public (blog images for e.g.)
 
 export default defineNuxtConfig({
   modules: ["@nuxt/content"],
