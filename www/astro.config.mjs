b7d98b27cc41cb29193494bcd0a6ad8b9d214689
Luke Oliff <luke@lukeoliff.com>
2022-06-12 21:26:42 +0100

    works

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,11 +1,21 @@
 import { defineConfig } from "astro/config";
 
+const globalPanel = function () {
+  const integration = {
+    name: "import-panel",
+    hooks: {
+      "astro:config:setup": ({ injectScript }) => {
+        injectScript(
+          "page-ssr",
+          'import Panel from "./src/components/layout/Panel.astro"; global.Panel = Panel;'
+        );
+      },
+    },
+  };
+  return integration;
+};
+
 // https://astro.build/config
 export default defineConfig({
-  "astro:config:setup": ({ injectScript }) => {
-    injectScript(
-      "page-ssr",
-      'import Panel from "./src/components/layout/Panel.astro"; global.Panel = Panel;'
-    );
-  },
+  integrations: [globalPanel()],
 });
