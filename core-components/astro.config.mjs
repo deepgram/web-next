2ebd494eb1cfae98c3332afe3f156b890394f7da
Sandra Rodgers <sandra.rodgers@deepgram.com>
2022-06-14 12:36:12 -0500

    added tailwind to core-components / update button

--- core-components/astro.config.mjs
+++ core-components/astro.config.mjs
@@ -1,8 +1,4 @@
-import { defineConfig } from 'astro/config';
-import vue from "@astrojs/vue";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [vue(), tailwind()]
-});
+export default defineConfig({});
