c9db52b2b24c27e599541e1f4085a4b4e4f84f0b
Luke Oliff <luke@lukeoliff.com>
2022-06-19 11:51:37 +0100

    stuff

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from "astro/config";
 import globals from "./src/plugins/globalSsrComponents.mjs";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [globals()],
+  integrations: [globals(), tailwind()],
+  vite: {
+    resolve: {
+      preserveSymlinks: true, // required for dev to read contents of symlinked directory
+    },
+  },
 });
