64503f32803b8e4cebf7da3f0fbbb181bdf01ced
Luke Oliff <luke@lukeoliff.com>
2022-06-22 14:01:29 +0100

    duplicate www for blog

---
+++ blog/src/plugins/globalSsrComponents.mjs
@@ -0,0 +1,28 @@
+import glob from "glob";
+import path from "path";
+
+const cwd = process.cwd();
+
+export default function globals() {
+  /** @type {import('astro').AstroIntegration} */
+
+  const integration = {
+    name: "astro-global-ssr-components",
+    hooks: {
+      "astro:config:setup": ({ injectScript }) => {
+        glob("src/components/**/*.page-ssr.astro", {}, function (er, files) {
+          files.forEach((file) => {
+            const info = path.parse(file);
+            const name = info.name.replace(".page-ssr", "");
+            injectScript(
+              "page-ssr",
+              `import ${name} from "${cwd}/${file}"; global.${name} = ${name};`
+            );
+          });
+        });
+      },
+    },
+  };
+
+  return integration;
+}
