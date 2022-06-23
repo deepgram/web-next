5eff8f85b2348fb7a7acf02fa3fb0fd7f85d7da9
Sandra Rodgers <sandra.rodgers@deepgram.com>
2022-06-23 17:02:50 -0500

    building homepage

--- www/src/components/content/demo/DemoTranscription.vue
+++ www/src/components/content/demo/DemoTranscription.vue
@@ -1,5 +1,25 @@
 <script setup></script>
 
 <template>
-  <div>DEMO TRANSCRIPTION</div>
+  <div class="demo-transcription">
+    <div>TOGGLE</div>
+    <p>Transcribe your voice in real-time.</p>
+    <p>Click the mic to give it a try.</p>
+    <div class="flex justify-between">
+      <div>
+        <button>microphone</button>
+        <button>dots</button>
+      </div>
+      <div>Language select</div>
+    </div>
+  </div>
 </template>
+
+<style>
+.demo-transcription {
+  @apply flex flex-col;
+  @apply w-8/12;
+  /* TODO Change to a blur effect: */
+  background-color: #181d24; 
+}
+</style>
