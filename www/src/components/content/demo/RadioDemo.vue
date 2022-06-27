fed09ecbc9245329d302db65580886459acf969b
Sandra Rodgers <sandra.rodgers@deepgram.com>
2022-06-27 15:25:59 -0500

    toggle and radio for transcription demo

---
+++ www/src/components/content/demo/RadioDemo.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  items: Array,
+  modelValue: String,
+});
+
+const picked = ref("One");
+const emit = defineEmits(["update:modelValue"]);
+</script>
+
+<template>
+  <div>
+    <div v-for="item in items" :key="item.id">
+      <input
+        type="radio"
+        :id="item.title"
+        :value="item.code"
+        v-model="picked"
+        @input="$emit('update:modelValue', $event.target.value)"
+      />
+      <label for="one">{{ item.title }}</label>
+      <p>{{ item.text }}</p>
+    </div>
+  </div>
+</template>
