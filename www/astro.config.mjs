5b0383235322397cd46e766e917fff27b3d99dfe
Luke Oliff <luke@lukeoliff.com>
2022-07-07 17:22:07 +0100

    fix astro/tailwind config to stop importing base styles twice

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -5,7 +5,14 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), globals(), vue()],
+  integrations: [
+    tailwind({
+      // Disable injecting a basic `base.css` import on every page.
+      config: { applyBaseStyles: false },
+    }),
+    globals(),
+    vue(),
+  ],
   vite: {
     resolve: {
       preserveSymlinks: true, // required for dev to read contents of symlinked directory
