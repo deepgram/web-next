a002d8eb9e049c664d617b798ea23853bb058c37
Luke Oliff <luke@lukeoliff.com>
2022-07-07 16:23:08 +0100

    restore component library

---
+++ core-components/components/button/MicButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <button>
+    <div>
+      <div>Mic Button</div>
+    </div>
+  </button>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ core-components/demo/astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [vue(), tailwind()],
+});
