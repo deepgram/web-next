4d851b550010aaac63673fadaa38d4c8034f96c2
Luke Oliff <luke@lukeoliff.com>
2022-07-15 09:36:53 +0100

    prettier configured

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,21 +1,21 @@
-import { defineConfig } from "astro/config";
-import globals from "./src/plugins/globalSsrComponents.mjs";
-import tailwind from "@astrojs/tailwind";
-import vue from "@astrojs/vue";
+import { defineConfig } from 'astro/config';
+import globals from './src/plugins/globalSsrComponents.mjs';
+import tailwind from '@astrojs/tailwind';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    globals(),
-    tailwind({
-      // Disable injecting a basic `base.css` import on every page.
-      config: { applyBaseStyles: false },
-    }),
-    vue(),
-  ],
-  vite: {
-    resolve: {
-      preserveSymlinks: true, // required for dev to read contents of symlinked directory
-    },
-  },
+	integrations: [
+		globals(),
+		tailwind({
+			// Disable injecting a basic `base.css` import on every page.
+			config: { applyBaseStyles: false },
+		}),
+		vue(),
+	],
+	vite: {
+		resolve: {
+			preserveSymlinks: true, // required for dev to read contents of symlinked directory
+		},
+	},
 });
--- www/src/components/content/demo/product/ToggleTwo.vue
+++ www/src/components/content/demo/product/ToggleTwo.vue
@@ -1,93 +1,80 @@
 <script setup>
-import { ref } from "vue";
+import { ref } from 'vue';
 
 const props = defineProps({
-  demoItems: Array,
-  modelValue: String,
+	demoItems: Array,
+	modelValue: String,
 });
 
 const picked = ref(0);
-const emit = defineEmits(["update:modelValue"]);
+const emit = defineEmits(['update:modelValue']);
 </script>
 
 <template>
-  <div class="radio-group">
-    <div class="item" v-for="item in demoItems" :key="item.id">
-      <label :for="item.id">
-        <div
-          class="label-text"
-          :class="item.id === picked ? 'picked' : 'not-picked'"
-        >
-          <div class="title">
-            {{ item.title }}
-          </div>
-          <div class="details">{{ item.details }}</div>
-          <a href="{href}" class="hyperlink flex items-center mt-4"
-            ><span
-              class="pr-1"
-              :class="item.id === picked ? 'picked' : 'not-picked'"
-              >{{ item.buttonText }}</span
-            ></a
-          >
-        </div>
-        <input
-          type="radio"
-          :id="item.title"
-          :value="item.id"
-          v-model="picked"
-          @input="$emit('update:modelValue', $event.target.value)"
-        />
-      </label>
-    </div>
-  </div>
+	<div class="radio-group">
+		<div class="item" v-for="item in demoItems" :key="item.id">
+			<label :for="item.id">
+				<div class="label-text" :class="item.id === picked ? 'picked' : 'not-picked'">
+					<div class="title">
+						{{ item.title }}
+					</div>
+					<div class="details">{{ item.details }}</div>
+					<a href="{href}" class="hyperlink mt-4 flex items-center"
+						><span class="pr-1" :class="item.id === picked ? 'picked' : 'not-picked'">{{ item.buttonText }}</span></a
+					>
+				</div>
+				<input type="radio" :id="item.title" :value="item.id" v-model="picked" @input="$emit('update:modelValue', $event.target.value)" />
+			</label>
+		</div>
+	</div>
 </template>
 
 <style scoped>
 .title {
-  @apply font-semibold text-2xl;
-  @apply mb-4;
+	@apply text-2xl font-semibold;
+	@apply mb-4;
 }
 
-input[type="radio"] {
-  position: absolute;
-  opacity: 0;
-  height: 100%;
-  left: 0;
-  margin: 0;
-  padding: 0;
-  top: 0;
-  width: 100%;
-  cursor: pointer;
+input[type='radio'] {
+	position: absolute;
+	opacity: 0;
+	height: 100%;
+	left: 0;
+	margin: 0;
+	padding: 0;
+	top: 0;
+	width: 100%;
+	cursor: pointer;
 }
 
 label {
-  position: relative;
-  @apply h-full pr-6;
-  @apply flex items-center;
+	position: relative;
+	@apply h-full pr-6;
+	@apply flex items-center;
 }
 
 svg {
-  @apply m-5;
+	@apply m-5;
 }
 
 .radio-group {
-  @apply mb-10;
+	@apply mb-10;
 }
 
 label {
-  @apply px-5;
+	@apply px-5;
 }
 
 .label-text {
-  /* @apply w-11/12; */
+	/* @apply w-11/12; */
 }
 
 .item {
-  @apply border-lightIris border-l-2;
-  @apply mt-14 mb-3;
+	@apply border-lightIris border-l-2;
+	@apply mt-14 mb-3;
 }
 
 .not-picked {
-  @apply text-casper;
+	@apply text-casper;
 }
 </style>
