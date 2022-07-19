605032753540cffea1e276779375f900fd317c22
Luke Oliff <luke@lukeoliff.com>
2022-07-19 09:45:32 +0100

    shared astro and blog using shared astro components

--- shared/astro.config.mjs
+++ shared/astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [
+		tailwind({
+			// Disable injecting a basic `base.css` import on every page.
+			config: { applyBaseStyles: false },
+		}),
+	],
+	vite: {
+		resolve: {
+			preserveSymlinks: true, // required for dev to read contents of symlinked directory
+		},
+	},
+});
