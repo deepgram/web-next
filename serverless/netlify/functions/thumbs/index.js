e24161aab750e52fc9546845c24d620878f3d127
Brian Barrow <briancbarrow@gmail.com>
2022-07-25 15:28:40 -0600

    added axios, added zapier webhook

--- serverless/netlify/functions/thumbs/index.js
+++ serverless/netlify/functions/thumbs/index.js
@@ -1,7 +1,14 @@
+const axios = require('axios')
+
 exports.handler = async function (event, context) {
-  console.log("event:", JSON.parse(event.body))
+  const {
+    data: { data: streams },
+  } = await axios.post(
+    `https://hooks.zapier.com/hooks/catch/11179787/bgv9wf1/`,
+    event.body
+    
+  )
     return {
       statusCode: 200,
-      body: "Thumbs",
     }  
 }
