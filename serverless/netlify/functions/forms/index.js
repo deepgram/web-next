bed38b2ca1a0f7944e8f98b83b7c2429b2b0b65d
Brian Barrow <briancbarrow@gmail.com>
2022-07-28 14:15:10 -0600

    added heading for cors

--- serverless/netlify/functions/forms/index.js
+++ serverless/netlify/functions/forms/index.js
@@ -1,13 +1,16 @@
 const axios = require("axios");
 
 exports.handler = async function (event, context) {
-	const body = JSON.parse(event.body);
-	const hookId = body.hookId;
-	const url = `${process.env.ZAP_HOOK_URL}${hookId}`;
-	const {
-		data: { data: streams },
-	} = await axios.post(url, event.body);
-	return {
-		statusCode: 200,
-	};
+  const body = JSON.parse(event.body);
+  const hookId = body.hookId;
+  const url = `${process.env.ZAP_HOOK_URL}${hookId}`;
+  const {
+    data: { data: streams },
+  } = await axios.post(url, event.body);
+  return {
+    statusCode: 200,
+    headers: {
+      "access-control-allow-origin": "*",
+    },
+  };
 };
