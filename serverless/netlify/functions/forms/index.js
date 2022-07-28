fcd4d0226fb8b4634cfabcbeb13b00d5fdf979b9
Brian Barrow <briancbarrow@gmail.com>
2022-07-28 14:04:34 -0600

    Update serverless/netlify/functions/forms/index.js
    Co-authored-by: Luke Oliff <luke@lukeoliff.com>

--- serverless/netlify/functions/forms/index.js
+++ serverless/netlify/functions/forms/index.js
@@ -1,17 +1,13 @@
-const axios = require('axios')
+const axios = require("axios");
 
 exports.handler = async function (event, context) {
-  const body = JSON.parse(event.body)
-  const hookId = body.hookId
-  const url = `${process.env.ZAP_HOOK_URL}${hookId}`
-  const {
-    data: { data: streams },
-  } = await axios.post(
-    url,
-    event.body
-    
-  )
-    return {
-      statusCode: 200,
-    }  
-}
+	const body = JSON.parse(event.body);
+	const hookId = body.hookId;
+	const url = `${process.env.ZAP_HOOK_URL}${hookId}`;
+	const {
+		data: { data: streams },
+	} = await axios.post(url, event.body);
+	return {
+		statusCode: 200,
+	};
+};
