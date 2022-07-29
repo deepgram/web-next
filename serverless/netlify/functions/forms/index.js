1ec51b1a0658641e8fa7272d77818c1cda2e61ad
Brian Barrow <briancbarrow@gmail.com>
2022-07-29 13:34:58 -0600

    removed await

--- serverless/netlify/functions/forms/index.js
+++ serverless/netlify/functions/forms/index.js
@@ -4,9 +4,7 @@ exports.handler = async function (event, context) {
   const body = JSON.parse(event.body);
   const hookId = body.hookId;
   const url = `${process.env.ZAP_HOOK_URL}${hookId}`;
-  const {
-    data: { data: streams },
-  } = await axios.post(url, event.body);
+  axios.post(url, event.body);
   return {
     statusCode: 200,
     headers: {
