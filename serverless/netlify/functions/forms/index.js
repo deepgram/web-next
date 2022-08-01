b811d58181745e31a148c8998ae57e953f74dd4a
Brian Barrow <briancbarrow@gmail.com>
2022-08-01 12:14:58 -0600

    added logging and error handling to forms function

--- serverless/netlify/functions/forms/index.js
+++ serverless/netlify/functions/forms/index.js
@@ -1,14 +1,25 @@
 const axios = require("axios");
 
 exports.handler = async function (event, context) {
-  const body = JSON.parse(event.body);
-  const hookId = body.hookId;
+	const body = JSON.parse(event.body);
+	const hookId = body.hookId;
   const url = `${process.env.ZAP_HOOK_URL}${hookId}`;
-  axios.post(url, event.body);
-  return {
-    statusCode: 200,
-    headers: {
-      "access-control-allow-origin": "*",
-    },
-  };
+  console.log('url', url);
+  try {
+    await axios.post(url, event.body);
+    return {
+      statusCode: 200,
+      headers: {
+        "access-control-allow-origin": "*",
+      },
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,      
+      headers: {
+        "access-control-allow-origin": "*",
+      },
+    };
+  }
 };
