f7066afa0ba0132794458ad7cb59a034e893cce0
Sandra Rodgers <sandra.rodgers@deepgram.com>
2022-08-04 14:22:38 -0500

    updated deepgram_token function

--- serverless/netlify/functions/deepgram_token/index.js
+++ serverless/netlify/functions/deepgram_token/index.js
@@ -4,15 +4,16 @@ require("dotenv").config();
 const deepgram = new Deepgram(process.env.DEEPGRAM_API_KEY);
 const deepgramProjectId = process.env.DEEPGRAM_PROJECT_ID;
 
-exports.handler = async function (event, context) {
+exports.handler = async function () {
 	try {
-		const key = await deepgram.keys.create(deepgramProjectId, "Temp 404 key", ["member"], {
+		const key = await deepgram.keys.create(deepgramProjectId, "Temporary key", ["member"], {
 			timeToLive: 5,
 		});
 
 		return {
 			statusCode: 200,
 			body: JSON.stringify(key),
+			headers: { "access-control-allow-origin": "*" },
 		};
 	} catch (err) {
 		// eslint-disable-next-line no-console
@@ -20,6 +21,7 @@ exports.handler = async function (event, context) {
 
 		return {
 			statusCode: 500,
+			headers: { "access-control-allow-origin": "*" },
 		};
 	}
 };
