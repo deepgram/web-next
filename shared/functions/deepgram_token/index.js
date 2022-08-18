9fc45e5297dbfb50368e385d39d7a30c576305b7
Luke Oliff <luke@lukeoliff.com>
2022-08-18 18:22:49 +0100

    Use referrer and origin (#71)
    * add referer as alternative origin address
    
    * wtf add all the cors stuff

--- shared/functions/deepgram_token/index.js
+++ shared/functions/deepgram_token/index.js
@@ -8,19 +8,23 @@ exports.handler = async function (event) {
 	const cors = process.env.DEEPGRAM_SERVERLESS_CORS;
 	let corsOrigin = "*";
 
-	const eventOrigin = new URL(event.headers["origin"] || event.headers["referer"]);
+	const origin = event.headers["origin"] || event.headers["referer"];
 
-	if (cors) {
+	if (cors && origin) {
+		const eventOrigin = new URL(origin);
 		const corsDomains = [process.env.URL, process.env.DEPLOY_URL, ...cors.split(", ")];
 		const thisCors = corsDomains.indexOf(eventOrigin.origin);
 
 		if (thisCors < 0) {
 			return {
-				statusCode: 403,
+				statusCode: 401,
 				body: "Forbidden by allow-list",
 				headers: {
 					"Access-Control-Allow-Origin": "*",
-					"Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+					"Access-Control-Allow-Headers": "Content-Type",
+					"Access-Control-Allow-Methods": "GET, OPTIONS",
+					Allow: "GET, OPTIONS",
+					Vary: "Origin",
 				},
 			};
 		}
@@ -30,7 +34,10 @@ exports.handler = async function (event) {
 
 	const headers = {
 		"Access-Control-Allow-Origin": corsOrigin,
-		"Access-Control-Allow-Methods": "GET",
+		"Access-Control-Allow-Headers": "Content-Type",
+		"Access-Control-Allow-Methods": "GET, OPTIONS",
+		Allow: "GET, OPTIONS",
+		Vary: "Origin",
 	};
 
 	// Only allow GET
@@ -38,9 +45,7 @@ exports.handler = async function (event) {
 		return {
 			statusCode: 405,
 			body: "Method Not Allowed",
-			headers: {
-				Allow: "Get",
-			},
+			headers,
 		};
 	}
 
