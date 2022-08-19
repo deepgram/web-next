281b7801558c322d6a6ee12e6bd47aad03fa5e67
Luke Oliff <luke@lukeoliff.com>
2022-08-19 10:47:46 +0100

    tidy up functions for CORS allowances

--- shared/functions/transcribe-prerecorded/index.js
+++ shared/functions/transcribe-prerecorded/index.js
@@ -3,56 +3,53 @@ require("dotenv").config();
 
 const deepgram = new Deepgram(process.env.DEEPGRAM_API_KEY);
 
+const buildCorsHeaders = (event, methods) => {
+	// set default Access-Control-Allow-Origin to the current deployed URL
+	let corsOrigin = process.env.URL;
+
+	// get "allow-list" CORS domains from environment variable
+	const cors = process.env.DEEPGRAM_SERVERLESS_CORS;
+
+	// get "origin" or referer from request headers
+	const origin = event.headers["origin"] || event.headers["referer"];
+
+	// do we have an allow-list and origin?
+	if (cors && origin) {
+		// parse the origin as a URL
+		const eventOrigin = new URL(origin);
+
+		// get allow-list as an array
+		const corsDomains = [process.env.URL, process.env.DEPLOY_URL, ...cors.split(", ")];
+
+		// find the current request origin in the allow-list
+		const thisCors = corsDomains.indexOf(eventOrigin.origin);
+
+		// if in the allow-list, over-ride the Access-Control-Allow-Origin
+		if (thisCors > -1) {
+			corsOrigin = corsDomains[thisCors];
+		}
+	}
+
+	return {
+		"Access-Control-Allow-Origin": corsOrigin,
+		"Access-Control-Allow-Headers": "Content-Type",
+		"Access-Control-Allow-Methods": methods,
+		Allow: methods,
+		Vary: "Origin",
+	};
+};
+
 exports.handler = async function transcribePrerecorded(event) {
+	const headers = buildCorsHeaders(event, "POST, OPTIONS");
+
 	switch (event.httpMethod) {
 		case "OPTIONS":
 			return {
 				statusCode: 200,
 				body: "OK",
-				headers: {
-					"Access-Control-Allow-Origin": "*",
-					"Access-Control-Allow-Headers": "Content-Type",
-					"Access-Control-Allow-Methods": "POST, OPTIONS",
-					Allow: "POST, OPTIONS",
-					Vary: "Origin",
-				},
+				headers,
 			};
 		case "POST":
-			const cors = process.env.DEEPGRAM_SERVERLESS_CORS;
-			let corsOrigin = "*";
-
-			const origin = event.headers["origin"] || event.headers["referer"];
-
-			if (cors && origin) {
-				const eventOrigin = new URL(origin);
-				const corsDomains = [process.env.URL, process.env.DEPLOY_URL, ...cors.split(", ")];
-				const thisCors = corsDomains.indexOf(eventOrigin.origin);
-
-				if (thisCors < 0) {
-					return {
-						statusCode: 403,
-						body: "Forbidden by allow-list",
-						headers: {
-							"Access-Control-Allow-Origin": "*",
-							"Access-Control-Allow-Headers": "Content-Type",
-							"Access-Control-Allow-Methods": "POST, OPTIONS",
-							Allow: "POST, OPTIONS",
-							Vary: "Origin",
-						},
-					};
-				}
-
-				corsOrigin = corsDomains[thisCors];
-			}
-
-			const headers = {
-				"Access-Control-Allow-Origin": corsOrigin,
-				"Access-Control-Allow-Headers": "Content-Type",
-				"Access-Control-Allow-Methods": "POST, OPTIONS",
-				Allow: "POST, OPTIONS",
-				Vary: "Origin",
-			};
-
 			// format of body is now { url: "https:// etc" }
 			// but don't pass the parsed body to the function, to avoid abuse
 			const { url } = JSON.parse(event.body);
@@ -66,6 +63,7 @@ exports.handler = async function transcribePrerecorded(event) {
 					headers,
 				};
 			} catch (err) {
+				// eslint-disable-next-line no-console
 				console.log(err);
 
 				return {
@@ -78,13 +76,7 @@ exports.handler = async function transcribePrerecorded(event) {
 			return {
 				statusCode: 405,
 				body: "Method Not Allowed",
-				headers: {
-					"Access-Control-Allow-Origin": "*",
-					"Access-Control-Allow-Headers": "Content-Type",
-					"Access-Control-Allow-Methods": "POST, OPTIONS",
-					Allow: "POST, OPTIONS",
-					Vary: "Origin",
-				},
+				headers,
 			};
 	}
 };
