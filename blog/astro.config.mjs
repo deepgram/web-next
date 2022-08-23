c27326a2b1d8860f1ce0bdcb512e8d13796e1df3
Luke Oliff <luke@lukeoliff.com>
2022-08-23 19:08:29 +0100

    remove rehype-autolink-headings

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import autolinkHeadings from "rehype-autolink-headings";
 import globals from "./src/plugins/globalSsrComponents.mjs";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
@@ -15,12 +14,6 @@ export default defineConfig({
 		}),
 		vue(),
 	],
-	markdown: {
-		rehypePlugins: ["rehype-autolink-headings"],
-		// rehypePlugins: [autolinkHeadings],
-		// rehypePlugins: [["rehype-autolink-headings", { behavior: "prepend" }]],
-		// or rehypePlugins: [[autolinkHeadings, { behavior: "prepend" }]],
-	},
 	vite: {
 		resolve: {
 			preserveSymlinks: true, // required for dev to read contents of symlinked directory
