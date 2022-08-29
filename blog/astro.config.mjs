24d0bc50b044bf34452843ff9fc8c6de40536052
Luke Oliff <luke@lukeoliff.com>
2022-08-29 12:08:15 +0100

    undo this

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -6,7 +6,7 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-	site: import.meta.env.URL,
+	site: process.env.URL,
 	integrations: [
 		sitemap(),
 		globals(),
