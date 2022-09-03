79d69fef20b7d620c55da2104e2c93e2aa546d58
Michael Jolley <mike@sparcapp.io>
2022-09-03 14:54:06 -0500

    working on post_announcement action

--- actions/post_announcement/index.js
+++ actions/post_announcement/index.js
@@ -5,32 +5,42 @@ const httpm = require("@actions/http-client");
 const { SHORT_URL_FUNCTION_URL } = process.env;
 
 try {
-	// Identify files & slugs to generate
-	const files = core.getInput("files");
+    // Identify files & slugs to generate
+    const inputfiles = core.getInput("files");
 
-	console.dir(files);
+    const files = inputfiles.split[' ']
+    console.dir(files);
+
+    for (const file of files) {
+        await processPost(file);
+    }
+
 } catch (error) {
-	core.setFailed(error.message);
+    core.setFailed(error.message);
+}
+
+const processPost = async (filename) => {
+    console.log(filename)
 }
 
 const getShortUrl = async (longUrl) => {
-	const https = new httpm.HttpClient();
-	const result = await https.post(
-		SHORT_URL_FUNCTION_URL,
-		JSON.stringify({
-			longUrl,
-			honeypot: "",
-		}),
-		{
-			headers: {
-				Accept: "application/json",
-				"Content-Type": "application/json",
-			},
-		}
-	);
+    const https = new httpm.HttpClient();
+    const result = await https.post(
+        SHORT_URL_FUNCTION_URL,
+        JSON.stringify({
+            longUrl,
+            honeypot: "",
+        }),
+        {
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+        }
+    );
 
-	const body = await result.readBody();
-	const urls = JSON.parse(body);
+    const body = await result.readBody();
+    const urls = JSON.parse(body);
 
-	return urls.shortUrl;
+    return urls.shortUrl;
 };
--- content/blog/posts/2021-state-of-automatic-speech-recognition-infographic/index.md
+++ content/blog/posts/2021-state-of-automatic-speech-recognition-infographic/index.md
@@ -1,6 +1,6 @@
 ---
 title: "2021 State of Automatic Speech Recognition Infographic"
-description: "The 2021 State of Automatic Speech Recognition (ASR) report details where enterprises are currently using this technology and where they see the most value.  Find out why 85% of enterprises say that ASR is important or very important to their future strategy."
+description: "The 2021 State of Automatic Speech Recognition (ASR) report details where enterprises are currently using this technology and where they see the most value. Find out why 85% of enterprises say that ASR is important or very important to their future strategy."
 date: 2021-03-03
 cover: https://res.cloudinary.com/deepgram/image/upload/v1661981248/blog/2021-state-of-automatic-speech-recognition-infographic/2021-state-of-asr-infogfx%402x.jpg
 authors:
