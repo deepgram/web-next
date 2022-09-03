135e028c060d640c375d24649621d3d75d9a70fc
Michael Jolley <mike@sparcapp.io>
2022-09-03 13:28:18 -0500

    Fixing new action

--- actions/post_announcement/index.js
+++ actions/post_announcement/index.js
@@ -1,45 +1,43 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const httpm = require('@actions/http-client')
+const core = require("@actions/core");
+const github = require("@actions/github");
+const httpm = require("@actions/http-client");
 
-const {
-    SHORT_URL_FUNCTION_URL
-} = process.env;
+const { SHORT_URL_FUNCTION_URL } = process.env;
 
 try {
+	// Identify files & slugs to generate
+	const files = github.context?.payload?.head_commit?.added;
 
-    // Identify files & slugs to generate
-    const files = github.context.payload.head_commit.added;
+	console.log(files);
 
-    console.log(files);
-
-    for (const file of files) {
-        if (file.indexOf('blog/posts/') >= 0 &&
-            file.indexOf('index.md') >= 0) { // blog post
-            console.log(`Found: ${file}`);
-        }
-    }
+	for (const file of files) {
+		if (file.indexOf("blog/posts/") >= 0 && file.indexOf("index.md") >= 0) {
+			// blog post
+			console.log(`Found: ${file}`);
+		}
+	}
 } catch (error) {
-    core.setFailed(error.message);
+	core.setFailed(error.message);
 }
 
 const getShortUrl = async (longUrl) => {
+	const https = new httpm.HttpClient();
+	const result = await https.post(
+		SHORT_URL_FUNCTION_URL,
+		JSON.stringify({
+			longUrl,
+			honeypot: "",
+		}),
+		{
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+		}
+	);
 
-    const https = new httpm.HttpClient();
-    const result = await https.post(SHORT_URL_FUNCTION_URL, JSON.stringify(
-        {
-            longUrl,
-            honeypot: ''
-        }
-    ), {
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-        }
-    });
+	const body = await result.readBody();
+	const urls = JSON.parse(body);
 
-    const body = await result.readBody();
-    const urls = JSON.parse(body);
-
-    return urls.shortUrl;
-}
+	return urls.shortUrl;
+};
