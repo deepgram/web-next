001881fb89580809c5e864367afec62481a1e7f3
Michael Jolley <mike@sparcapp.io>
2022-09-03 22:30:59 -0500

    Working on add-short-urls action

--- actions/add_short_urls/index.js
+++ actions/add_short_urls/index.js
@@ -1,12 +1,11 @@
-const core = require("@actions/core");
-const exec = require("@actions/exec");
-const httpm = require("@actions/http-client");
-const fs = require("fs");
-const { remark } = require("remark");
-const remarkParse = require("remark-parse");
-const remarkFrontmatter = require("remark-frontmatter");
-const yaml = require("yaml");
-const { Toolkit } = require("actions-toolkit");
+import core from "@actions/core";
+import exec from "@actions/exec";
+import httpm from "@actions/http-client";
+import fs from "fs";
+import { remark } from "remark";
+import remarkParse from "remark-parse";
+import remarkFrontmatter from "remark-frontmatter";
+import yaml from "yaml";
 
 let filesChanged = false;
 
@@ -14,106 +13,100 @@ const { SHORT_URL_FUNCTION_URL } = process.env;
 const utm_sources = ["share_link", "twitter", "linkedin", "reddit", "facebook"];
 const https = new httpm.HttpClient();
 
-Toolkit.run(async (tools) => {
-	{
-		try {
-			const filterChildren = (opts) => {
-				return (tree) => {
-					return Object.assign({}, tree, {
-						children: tree.children.filter(opts.filter),
-					});
-				};
-			};
+const filterChildren = (opts) => {
+	return (tree) => {
+		return Object.assign({}, tree, {
+			children: tree.children.filter(opts.filter),
+		});
+	};
+};
 
-			const processPost = async (filename) => {
-				const slug = filename.split("/")[3];
-				const postUrl = `https://blog.deepgram.com/${slug}/`;
+const processPost = async (filename) => {
+	const slug = filename.split("/")[3];
+	const postUrl = `https://blog.deepgram.com/${slug}/`;
 
-				const orig = fs.readFileSync(filename);
+	const orig = fs.readFileSync(filename);
 
-				const processor = remark()
-					.use(remarkParse)
-					.use(remarkFrontmatter, ['yaml']);
+	const processor = remark()
+		.use(remarkParse)
+		.use(remarkFrontmatter, ['yaml']);
 
-				let originalTree = processor.parse(orig);
-				const transformedTree = processor.runSync(originalTree);
+	let originalTree = processor.parse(orig);
+	const transformedTree = processor.runSync(originalTree);
 
-				tools.log(transformedTree.children.map((m) => m.type).join(","));
+	const yamlNode = transformedTree.children.find((c) => c.type === "yaml");
+	const outputProcessor = processor().use(filterChildren, { filter: (c) => c.type !== "yaml" });
 
-				const yamlNode = transformedTree.children.find((c) => c.type === "yaml");
-				const outputProcessor = processor().use(filterChildren, { filter: (c) => c.type !== "yaml" });
+	const outputTree = outputProcessor.runSync(transformedTree);
+	const newMdText = outputProcessor.stringify(outputTree);
 
-				const outputTree = outputProcessor.runSync(transformedTree);
-				const newMdText = outputProcessor.stringify(outputTree);
+	const yml = yaml.parse(yamlNode.value);
+	const shorturls = yml.shorturls || {};
 
-				const yml = yaml.parse(yamlNode.value);
-				const shorturls = yml.shorturls || {};
+	for (const source of utm_sources) {
+		if (!shorturls[source.replace("_link", "")]) {
+			shorturls[source.replace("_link", "")] = await getShortUrl(`${postUrl}?utm_medium=share&utm_source=${source}&utm_campaign=blog&utm_content=${slug}`);
+			filesChanged = true;
+		}
+	}
 
-				for (const source of utm_sources) {
-					if (!shorturls[source.replace("_link", "")]) {
-						shorturls[source.replace("_link", "")] = await getShortUrl(`${postUrl}?utm_medium=share&utm_source=${source}&utm_campaign=blog&utm_content=${slug}`);
-						filesChanged = true;
-					}
-				}
+	yml.shorturls = shorturls;
 
-				yml.shorturls = shorturls;
-
-				let content = `---
+	let content = `---
 ${yaml.stringify(yml)}---
 
 ${newMdText}
 `;
-				fs.writeFileSync(filename, content);
-			};
+	fs.writeFileSync(filename, content);
+};
 
-			const getShortUrl = async (longUrl) => {
-				const result = await https.postJson(
-					SHORT_URL_FUNCTION_URL,
-					{
-						longUrl,
-						honeypot: "",
-					},
-					{
-						headers: {
-							"Content-Type": "application/json",
-						},
-					}
-				);
-
-				if (result.result) {
-					return result.result?.shortUrl;
-				}
-
-				return undefined;
-			};
-
-			const commitChanges = async (slug) => {
-				// git login & pull
-				await exec("git", ["config", "--local", "user.name", "Short Url Action"]);
-				await exec("git", ["config", "--local", "user.email", "devrel@deepgram.com"]);
-				await exec("git", ["pull", "origin", "main", "--tags"]);
-				await exec("git", ["add", "."]);
-				await exec("git", ["commit", "--no-commit-hooks", "-m", `Adding short urls for blog posts`]);
-
-				// push new version
-				await exec("git", ["push", "origin", `HEAD:main`]);
-			};
-
-			// Identify files & slugs to generate
-			const inputfiles = core.getInput("files");
-
-			const files = inputfiles.split(" ");
-
-			for (const file of files) {
-				await processPost(file);
-			}
-
-			// If we made changes, we need to commit and push
-			if (filesChanged) {
-				await commitChanges();
-			}
-		} catch (error) {
-			core.setFailed(error.message);
+const getShortUrl = async (longUrl) => {
+	const result = await https.postJson(
+		SHORT_URL_FUNCTION_URL,
+		{
+			longUrl,
+			honeypot: "",
+		},
+		{
+			headers: {
+				"Content-Type": "application/json",
+			},
 		}
+	);
+
+	if (result.result) {
+		return result.result?.shortUrl;
 	}
-});
+
+	return undefined;
+};
+
+const commitChanges = async (slug) => {
+	// git login & pull
+	await exec("git", ["config", "--local", "user.name", "Short Url Action"]);
+	await exec("git", ["config", "--local", "user.email", "devrel@deepgram.com"]);
+	await exec("git", ["pull", "origin", "main", "--tags"]);
+	await exec("git", ["add", "."]);
+	await exec("git", ["commit", "--no-commit-hooks", "-m", `Adding short urls for blog posts`]);
+
+	// push new version
+	await exec("git", ["push", "origin", `HEAD:main`]);
+};
+
+try {
+	// Identify files & slugs to generate
+	const inputfiles = core.getInput("files");
+
+	const files = inputfiles.split(" ");
+
+	for (const file of files) {
+		await processPost(file);
+	}
+
+	// If we made changes, we need to commit and push
+	if (filesChanged) {
+		await commitChanges();
+	}
+} catch (error) {
+	core.setFailed(error.message);
+}
