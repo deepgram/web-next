856ce33c681e9ba783bd034c64f3b8cec2fe9407
Luke Oliff <luke@lukeoliff.com>
2022-09-07 10:50:52 +0100

    add redirects to forms function (and allow form submission AND json)

--- shared/functions/forms/index.js
+++ shared/functions/forms/index.js
@@ -1,37 +1,49 @@
 const axios = require("axios");
+const querystring = require("node:querystring");
 
 exports.handler = async function (event, context) {
-	const body = JSON.parse(event.body);
-	const honeypot = body.honeypot;
-	if (honeypot !== "") {
-		return {
-			statusCode: 200,
-			headers: {
-				"access-control-allow-origin": "*",
-			},
-		};
-	}
-	const hookId = body.hookId;
+	let body = {};
 
-	const url = `${process.env.ZAP_HOOK_URL}${hookId}`;
+	console.log(event);
 
 	try {
-		await axios.post(url, event.body);
-
-		return {
-			statusCode: 200,
-			headers: {
-				"access-control-allow-origin": "*",
-			},
-		};
-	} catch (err) {
-		console.log(err);
-
-		return {
-			statusCode: 500,
-			headers: {
-				"access-control-allow-origin": "*",
-			},
-		};
+		body = JSON.parse(event.body);
+	} catch (e) {
+		body = querystring.parse(event.body);
 	}
+
+	let { honeypot, hook, redirect, ...payload } = body;
+	const url = `${process.env.ZAP_HOOK_URL}${hook}`;
+
+	let statusCode;
+	let headers = {
+		"access-control-allow-origin": "*",
+	};
+
+	if (honeypot !== "") {
+		console.log("honeypot triggered");
+
+		statusCode = redirect ? 302 : 200; // fake 200
+		redirect = event.headers.referer;
+	} else {
+		try {
+			await axios.post(url, payload);
+
+			statusCode = redirect ? 302 : 200;
+		} catch (err) {
+			console.log(err);
+
+			statusCode = redirect ? 302 : 500;
+			redirect = `${event.headers.referer}?error=500`;
+		}
+	}
+
+	if (redirect) {
+		headers["Location"] = redirect;
+	}
+
+	return {
+		statusCode,
+		headers,
+	};
 };
