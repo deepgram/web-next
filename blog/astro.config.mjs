96a7397b6080bffd047b0b1167cda94070ff4ac9
Luke Oliff <luke@lukeoliff.com>
2022-09-08 09:18:08 +0100

    Add search for desktop and mobile (#100)
    * search working for mobile and desktop nav
    
    * add react to the other sites too
    
    * fix builds
    
    * everything is broken
    
    * blah
    
    * rename ids to make them easier to debug later

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -3,6 +3,8 @@ import globals from "./src/plugins/globalSsrComponents.mjs";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
+import vitePreact from "@preact/preset-vite";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,11 +14,15 @@ export default defineConfig({
 		globals(),
 		tailwind({
 			// Disable injecting a basic `base.css` import on every page.
-			config: { applyBaseStyles: false },
+			config: {
+				applyBaseStyles: false,
+			},
 		}),
 		vue(),
+		preact(),
 	],
 	vite: {
+		plugins: [vitePreact()],
 		resolve: {
 			preserveSymlinks: true, // required for dev to read contents of symlinked directory
 		},
