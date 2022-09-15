294dbe4723154a50099ac318c99cc8e8b5371fce
Luke Oliff <luke@lukeoliff.com>
2022-09-15 12:03:41 +0100

    save old site

---
+++ www-old/astro.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from "astro/config";
+import globals from "./src/plugins/globalSsrComponents.mjs";
+import tailwind from "@astrojs/tailwind";
+import vue from "@astrojs/vue";
+import vitePreact from "@preact/preset-vite";
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
+export default defineConfig({
+	site: process.env.URL,
+	integrations: [
+		globals(),
+		tailwind({
+			// Disable injecting a basic `base.css` import on every page.
+			config: { applyBaseStyles: false },
+		}),
+		vue(),
+		preact(),
+	],
+	vite: {
+		plugins: [vitePreact()],
+		resolve: {
+			preserveSymlinks: true, // required for dev to read contents of symlinked directory
+		},
+	},
+});
