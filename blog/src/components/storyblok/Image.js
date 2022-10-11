e9a8d5bdf329ad9ea18cb5ee0066ed60afeed3b8
Brian Barrow <briancbarrow@gmail.com>
2022-10-11 14:24:32 -0600

    got storyblok working with separate richtext and custom components

--- blog/src/components/storyblok/Image.js
+++ blog/src/components/storyblok/Image.js
@@ -1,5 +1,6 @@
 const Image = ({ blok }) => {
-	return <img src={blok.src} alt={blok.alt} style={blok.style + "margin-bottom: 20px;"} />;
+	// return <img src={blok.src} alt={blok.alt} style={blok.style + "margin-bottom: 20px;"} />;
+	return <img {...blok} />;
 };
 
 export default Image;
