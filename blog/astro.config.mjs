528d851cbe22feb0b035b5ae63badfb1455e0f07
Sandra Rodgers <sandra.rodgers@deepgram.com>
2022-10-31 11:56:10 -0500

    netlify adapter

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -6,59 +6,59 @@ import vue from "@astrojs/vue";
 import vitePreact from "@preact/preset-vite";
 import preact from "@astrojs/preact";
 import storyblok from "@storyblok/astro";
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as dotenv from 'dotenv';
+import netlify from "@astrojs/netlify/edge-functions";
+dotenv.config();
+
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.URL,
-	integrations: [
-		sitemap(),
-		globals(),
-		tailwind({
-			// Disable injecting a basic `base.css` import on every page.
-			config: {
-				applyBaseStyles: false,
-			},
-		}),
-		vue(),
-		preact(),
-		storyblok({
-			// accessToken: "DQ5PVW2Hi8ZeawcnLc3nvQtt",
-			accessToken: "DQ5PVW2Hi8ZeawcnLc3nvQtt",
-			apiOptions: {
-				cache: { clear: "auto", type: "memory" },
-				region: "us",
-			},
-			bridge: true,
-			components: {
-				// page: "storyblok/Page",
-				// feature: "storyblok/Feature",
-				// grid: "storyblok/Grid",
-				// teaser: "storyblok/Teaser",
-				Author: "shared/components/cards/Author",
-				RichTextSection: "components//storyblok/RichText",
-				Iframe: "components/storyblok/Iframe",
-				Image: "components/storyblok/Image",
-				whitepaperLink: "shared/components/global/WhitepaperPromoSB",
-				Panel: "shared/components/global/Panel",
-				YouTube: "shared/components/global/YouTube",
-				Alert: "shared/components/global/Alert",
-				CodeEmbed: "shared/components/global/CodeEmbed",
-				Table: "shared/components/global/Table",
-				CodeGroup: "shared/components/code/CodeGroupSB",
-			},
-		}),
-	],
-	vite: {
-		plugins: [vitePreact()],
-		resolve: {
-			// required for dev to read contents of symlinked directory
-			preserveSymlinks: true,
-		},
-	},
-	legacy: {
-		// Adds support for legacy Markdown features
-		astroFlavoredMarkdown: true,
-	},
+  output: 'server',
+  site: process.env.URL,
+  integrations: [sitemap(), globals(), tailwind({
+    // Disable injecting a basic `base.css` import on every page.
+    config: {
+      applyBaseStyles: false
+    }
+  }), vue(), preact(), storyblok({
+    // accessToken: "DQ5PVW2Hi8ZeawcnLc3nvQtt",
+    accessToken: "DQ5PVW2Hi8ZeawcnLc3nvQtt",
+    apiOptions: {
+      cache: {
+        clear: "auto",
+        type: "memory"
+      },
+      region: "us"
+    },
+    bridge: true,
+    components: {
+      // page: "storyblok/Page",
+      // feature: "storyblok/Feature",
+      // grid: "storyblok/Grid",
+      // teaser: "storyblok/Teaser",
+      Author: "shared/components/cards/Author",
+      RichTextSection: "components//storyblok/RichText",
+      Iframe: "components/storyblok/Iframe",
+      Image: "components/storyblok/Image",
+      whitepaperLink: "shared/components/global/WhitepaperPromoSB",
+      Panel: "shared/components/global/Panel",
+      YouTube: "shared/components/global/YouTube",
+      Alert: "shared/components/global/Alert",
+      CodeEmbed: "shared/components/global/CodeEmbed",
+      Table: "shared/components/global/Table",
+      CodeGroup: "shared/components/code/CodeGroupSB"
+    }
+  })],
+  vite: {
+    plugins: [vitePreact()],
+    resolve: {
+      // required for dev to read contents of symlinked directory
+      preserveSymlinks: true
+    }
+  },
+  legacy: {
+    // Adds support for legacy Markdown features
+    astroFlavoredMarkdown: true
+  },
+  adapter: netlify()
 });
