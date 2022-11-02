5a4bdd2c55110e09dea63832f4a80e3cf15bdf8f
Luke Oliff <luke@lukeoliff.com>
2022-11-02 16:07:45 +0000

    all of the things

---
+++ developers/astro.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import vue from "@astrojs/vue";
+import vitePreact from "@preact/preset-vite";
+import preact from "@astrojs/preact";
+import mdx from "@astrojs/mdx";
+
+export default defineConfig({
+	site: process.env.URL,
+	integrations: [
+		sitemap(),
+		tailwind({
+			// Disable injecting a basic `base.css` import on every page.
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+		vue(),
+		preact(),
+		mdx(),
+	],
+	vite: {
+		plugins: [vitePreact()],
+		resolve: {
+			// required for dev to read contents of symlinked directory
+			preserveSymlinks: true,
+		},
+	},
+});
