dd02ea3189f2df19039461461f8bc62487b7ab9e
Michael Jolley <mike@sparcapp.io>
2022-11-03 19:08:13 -0500

    Adding webhook security to storyblok

--- shared/functions/storyblok/index.js
+++ shared/functions/storyblok/index.js
@@ -1,11 +1,35 @@
 require("dotenv").config();
 const axios = require('axios');
+const crypto = require('crypto');
 
 const actions = ["published", "unpublished", "deleted"];
 const url = 'https://api.github.com/repos/deepgram/web-next/dispatches';
 const githubPAT = process.env.GITHUB_BUILD_PAT;
+const webhookSecret = process.env.STORYBLOK_WEBHOOK_SECRET;
+
+function verifySignature(body, signature) {
+	let bodyHmac = crypto.createHmac('sha1', webhookSecret)
+		.update(JSON.stringify(body))
+		.digest('hex');
+
+	if (bodyHmac !== signature) {
+		throw new Error('Signature mismatch!');
+	}
+}
 
 exports.handler = async function (event) {
+
+	const signature = event.headers['webhook-signature'];
+	const body = JSON.parse(event.body);
+
+	try {
+		verifySignature(body, signature);
+	} catch (err) {
+		return {
+			statusCode: 403,
+		};
+	}
+
 	// Only allow POST
 	if (event.httpMethod !== "POST") {
 		return {
@@ -17,7 +41,7 @@ exports.handler = async function (event) {
 		};
 	}
 
-	const { action } = JSON.parse(event.body);
+	const { action } = body;
 
 	if (actions.find((a) => a === action)) {
 		await axios.post(url, {
