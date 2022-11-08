7d98bc1d4710fa54362aedacb09b0eba2eaa2ad8
Brian Barrow <briancbarrow@gmail.com>
2022-11-08 11:13:35 -0700

    added vite no-external

--- developers/astro.config.mjs
+++ developers/astro.config.mjs
@@ -23,6 +23,7 @@ export default defineConfig({
 		image(),
 	],
 	vite: {
+		ssr: { noExternal: ["node-html-parser"] },
 		plugins: [vitePreact()],
 		resolve: {
 			// required for dev to read contents of symlinked directory
