09b00c2fec929a3e9e0fcc688d3f7d8cce6fa6ab
Brian Barrow <briancbarrow@gmail.com>
2022-11-16 15:10:36 -0700

    Added SideNave jsx components (#609)

---
+++ shared/src/shared/components/nav/SideNav.jsx
@@ -0,0 +1,28 @@
+import { h, Fragment } from 'preact';
+import { useState } from 'preact/hooks';
+import SideNavList from "./SideNavList";
+export default function SideNav({ children, navStructure, navTitle, pathname }) {
+	// const [childrenShow, setChildrenShow] = useState([]);
+	// function expandList(nav) {
+		
+	// 	setChildrenShow(oldArray => {
+	// 		const newArr = [...oldArray, nav.to]
+	// 	});
+	// }
+	// function collapseList(nav) {
+	// 	const newArray = childrenShow.filter(item => item !== nav.to);
+	// 	setChildrenShow(newArray);
+	// }
+	// function isExpanded(nav) {
+	// 	return childrenShow.includes(nav.to);
+	// }
+	return (
+		<>
+			<nav
+	class="side-nav bg-cloud30 p-4">
+				<h5 class="border-b border-b-cloud pb-2 text-base">{navTitle}</h5>
+				<SideNavList navStructure={navStructure} pathname={pathname}/>				
+			</nav>
+		</>
+	);
+}
