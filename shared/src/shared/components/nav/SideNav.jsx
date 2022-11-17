8890c16e55604da6fd35166be344e5c7f23922a5
Sandra Rodgers <sandra.rodgers@deepgram.com>
2022-11-17 17:24:30 -0600

    more sdks stuff

--- shared/src/shared/components/nav/SideNav.jsx
+++ shared/src/shared/components/nav/SideNav.jsx
@@ -18,7 +18,7 @@ export default function SideNav({ children, navStructure, navTitle, pathname }) 
 	// }
 	return (
 		<>
-			<nav class="side-nav bg-cloud30 p-8 ">
+			<nav class="side-nav bg-cloud30 p-8 h-full">
 				<h5 class="border-b border-b-cloud pb-2 text-base">{navTitle}</h5>
 				<SideNavList navStructure={navStructure} pathname={pathname}/>				
 			</nav>
