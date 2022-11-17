0b257967e2fd3e223cad383c7d600c42e23439cb
Michael Jolley <michael.jolley@deepgram.com>
2022-11-17 12:15:25 -0600

    Defaulting Storyblok bridge to false

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -28,7 +28,7 @@ export default defineConfig({
 				cache: { clear: "auto", type: "memory" },
 				region: "us",
 			},
-			bridge: true,
+			bridge: false,
 			components: {
 				// page: "storyblok/Page",
 				// feature: "storyblok/Feature",
