38ea6da7f05eaba0a82a8b2e249ef05cda72a58b
Brian Barrow <briancbarrow@gmail.com>
2022-11-22 11:56:18 -0700

    got collapsable side nav working

--- shared/src/shared/components/nav/SideNav.jsx
+++ shared/src/shared/components/nav/SideNav.jsx
@@ -2,29 +2,30 @@ import { h, Fragment } from 'preact';
 import { useState } from 'preact/hooks';
 import SideNavList from "./SideNavList";
 export default function SideNav({ children, navStructure, navTitle, pathname }) {
-	// const [childrenShow, setChildrenShow] = useState([]);
+	const [childrenShow, setChildrenShow] = useState([]);
 	const [mobileNavOpen, setMobileNavOpen] = useState(false);
 	function toggleNav() {
 		setMobileNavOpen(!mobileNavOpen);
 	}
-	// function expandList(nav) {
-	// 	setChildrenShow(oldArray => {
-	// 		const newArr = [...oldArray, nav.to]
-	// 	});
-	// }
-	// function collapseList(nav) {
-	// 	const newArray = childrenShow.filter(item => item !== nav.to);
-	// 	setChildrenShow(newArray);
-	// }
-	// function isExpanded(nav) {
-	// 	return childrenShow.includes(nav.to);
-	// }
+	function expandList(nav) {
+		setChildrenShow(oldArray => {
+			const newArr = [...oldArray, nav.to]
+			return newArr;
+		});
+	}
+	function collapseList(nav) {
+		const newArray = childrenShow.filter(item => item !== nav.to);
+		setChildrenShow(newArray);
+	}
+	function isExpanded(nav) {
+		return childrenShow.includes(nav.to);
+	}
 	return (
 		<>
 			<button class={`fixed md:hidden h-auto w-auto p-3 rounded-r-md bg-cloud z-20 ${mobileNavOpen ? 'right-0' : 'left-0'}`} onClick={toggleNav}>{mobileNavOpen ? "close nav" : "nav"}</button>
 			<nav class={`side-nav col-span-2 md:col-span-1 fixed w-full z-10 overflow-y-scroll md:relative md:block bg-mist md:bg-cloud30 p-8 h-full ${mobileNavOpen ? 'block' : 'hidden'}`}>
 				<h5 class="border-b border-b-cloud pb-2 text-base">{navTitle}</h5>
-				<SideNavList navStructure={navStructure} pathname={pathname}/>				
+				<SideNavList navStructure={navStructure} pathname={pathname} expandList={expandList} collapseList={collapseList} isExpandedFunc={isExpanded} />				
 			</nav>
 		</>
 	);
--- shared/src/shared/components/nav/SideNavList.jsx
+++ shared/src/shared/components/nav/SideNavList.jsx
@@ -1,5 +1,5 @@
 
-export default function SideNavList({ navStructure, pathname, className}) {
+export default function SideNavList({ navStructure, pathname, className, collapseList, expandList, isExpandedFunc }) {
 
 	return (
 		<ul className={className}>
@@ -7,16 +7,15 @@ export default function SideNavList({ navStructure, pathname, className}) {
 					return (
 						<li class={pathname.includes(nav.to) ? "active" : ""}>
 							<div class="navlink-container flex items-center">
-								{/* {nav.children && !isExpandedFunc(nav) && (
-									<span onClick={clickIt} class="expand-nav cursor-pointer">+</span>
+								{nav.children && !isExpandedFunc(nav) && (
+									<span onClick={() => expandList(nav)} class="expand-nav cursor-pointer">+</span>
 								)}
 								{nav.children && isExpandedFunc(nav) && (
 									<span onClick={() => collapseList(nav)} class="expand-nav cursor-pointer">-</span>
-								)} */}
-								{/* class={`hover:text-iris leading-5 ${pathname.endsWith(nav.to) ? "text-storm" : "text-darkCharcoal"} ${nav.children ? "font-semibold" : ""}`} */}
+								)}
 								<a href={nav.to} class={`hover:text-iris text-sm my-2 pl-3 py-1 text-darkCharcoal leading-5 ${pathname.endsWith(nav.to) ? "font-semibold" : ""} ${nav.children ? "font-semibold" : ""} ${pathname.endsWith(nav.to) && nav.children ? "border-l-4 border-meadow" : "" }`}>{nav.name}</a>
 							</div>
-								{nav.children && <SideNavList className={nav.children ? "border-l border-cloud" : ""} navStructure={nav.children} pathname={pathname} />}
+								{nav.children && <SideNavList className={`${nav.children ? "border-l border-cloud" : ""} ${isExpandedFunc(nav) ? "" : "hidden"}`} navStructure={nav.children} pathname={pathname} />}
 						</li>
 					)
 				})}
