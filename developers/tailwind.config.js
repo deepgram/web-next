698be5423d61324012d0044fae2372c1fbaa2beb
Brian Barrow <briancbarrow@gmail.com>
2022-12-07 14:15:50 -0700

    updated styling for api ref layout

--- developers/tailwind.config.js
+++ developers/tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
 			maxWidth: {
 				"screen-2xl": "96rem",
 			},
+			gridTemplateColumns: {
+				// Simple 16 column grid
+				'18': 'repeat(18, minmax(0, 1fr))',
+			},
+			gridColumn: {
+        'span-15': 'span 15 / span 15',
+      },
 			fontSize: {
 				"body-smallest": ["0.875rem", "1.125rem"],
 				body: ["1.125rem", "1.75rem"],
