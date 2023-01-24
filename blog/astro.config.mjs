4a05cda9b007429891aeeb712bcaca46c580e4c1
Brian Barrow <briancbarrow@gmail.com>
2023-01-24 11:09:31 -0700

    updating astro config for netlify builds

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -11,7 +11,9 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
 	output: "server",
-	adapter: netlify(),
+	adapter: netlify({
+    dist: new URL('./dist/', import.meta.url)
+  }),
 	site: process.env.URL,
 	integrations: [
 		sitemap(),
